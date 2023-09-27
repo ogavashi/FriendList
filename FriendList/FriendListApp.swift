f79f74a4acebdfa789bcf551d6ffa517fa151461
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2023-09-27 22:56:14 +0300

    Initial Commit

---
+++ FriendList/FriendListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendListApp.swift
+//  FriendList
+//
+//  Created by Oleg Gavashi on 27.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct FriendListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
