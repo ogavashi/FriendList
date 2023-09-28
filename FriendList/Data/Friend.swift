deed8157bc9dbf40e0ecf357cada2a1a479f3fef
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2023-09-28 19:00:45 +0300

    Add first struct

---
+++ FriendList/Data/Friend.swift
@@ -0,0 +1,13 @@
+//
+//  Friend.swift
+//  FriendList
+//
+//  Created by Oleg Gavashi on 28.09.2023.
+//
+
+import Foundation
+
+struct Friend: Identifiable {
+    let id: String;
+    let name: String
+}
